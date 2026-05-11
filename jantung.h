8dc9cdc0d52c626f4e5c956c1c08ec0deada9327
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:13:10 +0700

    class jantung

--- jantung.h
+++ jantung.h
@@ -3,3 +3,14 @@
 #include <iostream>
 using namespace std;
 
+class jantung {
+public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+
+#endif
