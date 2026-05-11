ac770da192e0671eaf4f64f5244c1195fdeadddc
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:00:50 +0700

    class ibu

--- Ibu.h
+++ Ibu.h
@@ -6,3 +6,22 @@
 #include "anak.h"
 using namespace std;
 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) 
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() 
+    {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
+
