f823d02ce0df03de80c73d9c885ee6581446a802
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:13:52 +0700

    manusia_h

--- manusia.h
+++ manusia.h
@@ -5,18 +5,3 @@
 #include "jantung.h"
 using namespace std;
 
-class manusia {
-public:
-    string name;
-    jantung varJantung;
-
-    manusia(string pName)
-        : name(pName) {
-        cout << name << " hidup\n";
-    }
-    ~manusia() {
-        cout << name << " mati\n";
-    }
-};
-
-#endif
