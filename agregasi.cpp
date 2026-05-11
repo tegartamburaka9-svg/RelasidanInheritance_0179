ef04698bcd875505711536fba20bac35ccb7f0a2
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:04:29 +0700

    agregasi ibu and anak

--- agregasi.cpp
+++ agregasi.cpp
@@ -9,20 +9,4 @@ int main() { // agregasi
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
 
-    varIbu->tambahAnak(varAnak1);
-    varIbu->tambahAnak(varAnak2);
-    varibu2->tambahAnak(varAnak3);
-    varibu2->tambahAnak(varAnak1);
-
-    varIbu->cetakAnak();
-    varibu2->cetakAnak();
-
-    delete varIbu;
-    delete varibu2;
-    delete varAnak1;
-    delete varAnak2;
-    delete varAnak3;
-
-    system("pause");
-    return 0;
-}
+  
