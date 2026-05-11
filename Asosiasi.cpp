023b304928ac458e618883c9abdca7ff0a7ce0c0
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:08:18 +0700

    public class pasien

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -4,3 +4,20 @@ using namespace std;
 
 class dokter;
 
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) 
+    {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien() 
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
