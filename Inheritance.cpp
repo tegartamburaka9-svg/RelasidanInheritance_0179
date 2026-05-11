969d1dd0a837f26f08cce51bce4cd93337ba2922
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-11 20:11:41 +0700

    class pelajar and int main

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -18,3 +18,26 @@ public:
     }
 };
 
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
+
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
